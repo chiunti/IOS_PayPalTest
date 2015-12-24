b9cb494b589206bedc37531551a5f768700a7ccd
chiunti <chiunti_abc@hotmail.com>
2015-12-24 16:22:42 -0600

    Initial Commit

---
+++ PayPal Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayPal Test
+//
+//  Created by admin on 24/12/15.
+//  Copyright © 2015 chiuntisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PayPal Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PayPal Test
+//
+//  Created by admin on 24/12/15.
+//  Copyright © 2015 chiuntisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
